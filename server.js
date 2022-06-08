4f9b87727e322cfb636e1353132abe71d475aa81
MonicaDalosto <monica.kerber@gmail.com>
2022-06-08 15:22:46 +0200

    added the cors() to the server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-// const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const productRouter = require("./routers/productRouter");
+const productRouter = require('./routers/productRouter');
 
 const PORT = 4000;
 app.use(express.json());
-// app.use(cors);
+app.use(cors());
 
-app.get("/hello", (req, res) => {
-  res.send("hello world");
+app.get('/hello', (req, res) => {
+  res.send('hello world');
 });
-app.use("/products", productRouter);
-app.listen(PORT, () => console.log("running on port 4000"));
+app.use('/products', productRouter);
+app.listen(PORT, () => console.log('running on port 4000'));
